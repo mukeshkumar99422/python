9ecdc740a3c3373eed04d29b12d7acb04ad8afb3
mukeshkumar99422 <156451966+mukeshkumar99422@users.noreply.github.com>
2024-03-20 15:38:14 +0530

    factorial.py

--- factorial.py
+++ factorial.py
@@ -1,8 +1,10 @@
 def fact(a):
+    if a<0:
+        return 0
     fact=1
     for i in range(1,a+1):
         fact=fact*i
-        
     return fact
+    
 a=int(input("Enter number:"))
 print("factorial of",a,"=",fact(a))
